3659a6a8eda56579450784b1c9d69ea1fd1de651
fwrhine <aghniaputriprawira@gmail.com>
2018-05-15 20:06:59 +0700

    modify controller

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -45,7 +45,7 @@ public class EchoController {
         String contentText = content.getText();
 
         String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        return new TextMessage(replyText.substring(1) + "yesseu");
     }
 
     @EventMapping
